699f2b09c881ac2c1bd567a5d1c50c64ea099e6a
Iker-Galante <ikergalant@gmail.com>
2024-09-20 15:11:18 -0300

    Most important changes

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 -soundhw pcspk
+qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 -device intel-hda -device hda-output
