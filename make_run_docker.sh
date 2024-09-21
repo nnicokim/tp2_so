5dde30bc15dbf72464ce37eba8ee6ad0402b9deb
Tobias Juhasz <108370047+juhasztobias@users.noreply.github.com>
2024-09-21 19:32:14 -0300

    Add script to run Docker and QEMU for debugging

---
+++ make_run_docker.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+docker run --rm -v $(PWD):/app -it agodio/itba-so-multi-platform:3.0 bash -c "cd /app/TP2/Toolchain && make all" 
+docker run --rm -v $(PWD):/app -it agodio/itba-so-multi-platform:3.0 bash -c "cd /app/TP2/ && make all"
+
+# Agrega un parametro para que se ejecute en modo debug
+if [ "$1" = "-w" ]; then
+    qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 -device intel-hda -device hda-output
+    # qemu-system-x86_64 -s -S -hda Image/x64BareBonesImage.qcow2 -m 512 -soundhw pcspkelseqemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 -soundhw pcspkfi
+else
+    qemu-system-x86_64 -M pc -drive file=Image/x64BareBonesImage.qcow2,format=qcow2 -m 512 -audiodev coreaudio,id=audio0 -machine pcspk-audiodev=audio0 
+fi
